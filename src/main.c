9aa5cf28e26bf254928768186dce555153f41022
jake-g <omonoid@uw.edu>
2015-11-22 13:23:53 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,5 @@
 /* main.c - chromatic guitar tuner
  *
- * Copyright (C) 2012 by Bjorn Roche
- *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted, provided
- * that the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation.  This software is provided "as is" without express or
- * implied warranty.
  *
  */
 
